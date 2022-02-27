acfbe8e3b01b73aa256ec998b31c0778f33c741e
dredly <dredly@gmail.com>
2022-02-27 15:46:00 +0000

    Completed part 1d

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,42 +1,29 @@
+// Left off at beginning of "Function that returns a function"
+
 import { useState } from "react";
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>The app is used by pressing the button</div>
-    )
-  }
-  return (
-    <div>Button press history: {props.allClicks.join(" ")}</div>
-  )
-}
+const Display = props => <div>{props.value}</div>
 
-const Button = ({ onClick, text }) => (
-  <button onClick={onClick}>{text}</button>
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
 )
 
 const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
+  const [value, setValue] = useState(10);
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'));
-    setLeft(left + 1);
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'));
-    setRight(right + 1);
+  const setToValue = (newValue) => {
+    console.log('value now', newValue);
+    setValue(newValue);
   }
 
   return (
     <div>
-      {left}
-      <Button onClick={handleLeftClick} text="left" />
-      <Button onClick={handleRightClick} text="right" />
-      {right}
-      <History allClicks={allClicks} />
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   );
 }
